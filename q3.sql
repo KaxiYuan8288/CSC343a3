af21b9c3da3a5315c342fe4215ae770fd1891e3e
KaxiYuan8288 <zihan.yuan@mail.utoronto.ca>
2023-11-24 19:59:03 -0500

    commit q3

---
+++ q3.sql
@@ -0,0 +1,44 @@
+SET search_path TO recording;
+DROP TABLE IF EXISTS q3 CASCADE;
+
+CREATE TABLE q3 (
+    person_id INTEGER NOT NULL,
+    name VARCHAR(255) NOT NULL
+);
+
+DROP VIEW IF EXISTS session_length, max_individual, max_group;
+
+CREATE VIEW session_length AS
+    SELECT SUM(length) AS session_length, session_id
+    FROM recording_segments
+    GROUP BY session_id;
+
+CREATE VIEW max_individual AS
+    SELECT i.person_id
+    FROM session_individuals i JOIN session_length l
+    ON i.session_id = l.session_id
+    WHERE l.session_length = (SELECT MAX(session_length) FROM session_length);
+
+CREATE VIEW max_group AS
+    SELECT m.member_id
+    FROM session_bands b
+    JOIN session_length l ON b.session_id = l.session_id
+    JOIN band_members m ON m.band_id = b.band_id
+    WHERE l.session_length = (SELECT MAX(session_length) FROM session_length);
+
+CREATE VIEW combined_max AS
+    SELECT member_id AS id
+    FROM max_group_session
+
+    UNION
+
+    SELECT person_id AS id
+    FROM max_individual_session;
+
+INSERT INTO q1(person_id, name)
+SELECT
+    p.person_id, p.name
+FROM combined_max c
+JOIN people p
+ON c.id = p.person_id;
+
